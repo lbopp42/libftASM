ec288d54f514369130818cf80b123298e02e44df
Lucas BOPP <lbopp@student.42.fr>
2019-05-29 12:19:43 +0200

    Fix putstr

--- srcs/ft_putstr.s
+++ srcs/ft_putstr.s
@@ -9,11 +9,11 @@ _ft_putstr:
 	mov		rbp, rsp
 	push	rdi
 	call	_ft_strlen
-	;mov		rdx, rax
-	;mov		rdi, 1
+	mov		rdx, rax
+	mov		rdi, 1
 	pop		rsi
-	;mov		rax, MACH_SYSCALL(4)
-	;syscall
+	mov		rax, MACH_SYSCALL(4)
+	syscall
 	mov		rsp, rbp
 	pop		rbp
 	ret
